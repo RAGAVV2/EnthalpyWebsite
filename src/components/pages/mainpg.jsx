f0ac174eaaa7154bc7887726e925c443a22c4902
ragav <ragav203@gmail.com>
2023-07-04 00:12:13 +0530

    add:content

--- src/components/pages/mainpg.jsx
+++ src/components/pages/mainpg.jsx
@@ -10,8 +10,8 @@ const HomeContent = () => {
         <p>
       <br/>
 <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        Certainly! Here's a large example paragraph:
-
+        <h2>Team Enthalpy</h2>
+<br/><br/>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, tortor sit amet congue commodo, elit mi pellentesque urna, nec bibendum tortor massa nec enim. Nunc dapibus pellentesque sapien, in tristique nulla feugiat a. Mauris ultricies, nisi id rhoncus consequat, tortor leo pellentesque sem, a consequat justo mauris non ligula. Phasellus quis semper mi, id fringilla purus. Sed eu aliquet nunc, eu bibendum nunc. Nam mollis est at magna euismod, eu tristique ligula consequat. Nullam bibendum semper arcu, in pellentesque eros volutpat eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin tristique nisi ac velit ullamcorper feugiat. Fusce fermentum tristique leo, sit amet consectetur mi efficitur vitae. Curabitur pulvinar interdum libero, ut efficitur purus ultricies et. Nulla ac pulvinar ligula. Integer semper sagittis elementum.
 
 Suspendisse aliquam, elit eu sollicitudin dignissim, erat dui venenatis ex, vitae commodo lacus nisi non ex. Vivamus et est malesuada, suscipit tortor id, vestibulum odio. Sed tincidunt nunc nec odio eleifend, sed pulvinar nunc fringilla. Donec in sagittis libero. Maecenas tristique dui id orci hendrerit, nec ullamcorper urna dignissim. Curabitur ut risus feugiat, fringilla metus eu, suscipit enim. Mauris sed felis bibendum, feugiat massa ut, mollis sapien. Integer faucibus odio id sem scelerisque facilisis. In lacinia orci sed aliquet hendrerit.
